1107da3e789a0793017fac1eaab9b3a39f697e84
Deepak8294 <92979838+Deepak8294@users.noreply.github.com>
2021-10-27 00:29:17 +0530

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -1,15 +1,14 @@
 public class Constructor {
-    int y, z, ;
-    //creating a parameterized constructor  
-    Constructor(int a, int b) {
-        int y = a;
-        int z = b;
-        System.out.println("subtraction two number");
-        System.out.println("y-x");
-    }
-    public static void main(String[] args) {
-        //creating objects and passing values 
-        Constructor r = new Constructor(30, 10);
-
-    }
+	int y, z;
+	//creating a parameterized constructor  
+	Constructor(int a, int b) {
+		int y = a;
+		int z = b;
+		System.out.println("subtraction two number");
+		System.out.println(y - z);
+	}
+	public static void main(String[] args) {
+		//creating objects and passing values 
+		Constructor r = new Constructor(30, 10);
+	}
 }
