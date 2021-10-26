7e93d34c9457fb0384d4ede8e8882dd4d5c6d843
Deepak8294 <92979838+Deepak8294@users.noreply.github.com>
2021-10-27 00:31:33 +0530

    Update DoWhile.java

--- DoWhile.java
+++ DoWhile.java
@@ -1,12 +1,10 @@
-    public class DoWhile {
-        public static void main(String[] args) {
-            int i = 1;
-            
-            do {
-                System.out.println(i); //code to be executed
-                i++;  //increment/decrement statement
-            }
-
-            while (i <= 10);  //condition
-        }
-    }
+ public class DoWhile {
+ 	public static void main(String[] args) {
+ 		int i = 1;
+ 		do {
+ 			System.out.println(i); //code to be executed
+ 			i++; //increment/decrement statement
+ 		}
+ 		while (i<= 10); //condition
+ 	}
+ }
