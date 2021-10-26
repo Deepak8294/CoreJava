9fed8488b00879ecabb83ee44de8769edf79db3d
Deepak8294 <92979838+Deepak8294@users.noreply.github.com>
2021-10-27 00:35:35 +0530

    Update StringMethods.java

--- StringMethods.java
+++ StringMethods.java
@@ -1,20 +1,19 @@
 public class StringMethods {
-    public static void main(String[] args) {
-        String s = "raj";
-        System.out.println(s.charAt(1)); //return the char value at the 1 index
-        //custom input string 2
-        s = s.concat("kumar");
-        //print combined string
-        System.out.println(s);
-        //concept of == operator
-        String s1 = "Hello";
-        String s2 = "Hello";
-        String s3 = new String("Hello");
-        System.out.println(s1 == s2); //true
-        System.out.println(s1 == s3); //false
-        System.out.println(s1.equals(s2)); //true
-        System.out.println(s1.equals(s3)); //true
+  public static void main(String[] args) {
+    String s = "raj";
+    System.out.println(s.charAt(1)); //return the char value at the 1 index
+    //custom input string 2
+    s = s.concat("kumar");
+    //print combined string
+    System.out.println(s);
+    //concept of == operator
+    String s1 = "Hello";
+    String s2 = "Hello";
+    String s3 = new String("Hello");
+    System.out.println(s1 == s2); //true
+    System.out.println(s1 == s3); //false
+    System.out.println(s1.equals(s2)); //true
+    System.out.println(s1.equals(s3)); //true
 
-
-    }
+  }
 }
