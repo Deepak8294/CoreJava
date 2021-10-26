94770a07e7eb4d423021b9a7664691591720908c
Deepak8294 <92979838+Deepak8294@users.noreply.github.com>
2021-10-27 00:36:48 +0530

    Update OperatorEmi.java

--- OperatorEmi.java
+++ OperatorEmi.java
@@ -1,21 +1,21 @@
 public class OperatorEmi {
-    public static void main(String args[]) {
+  public static void main(String args[]) {
 
-        double principal = 2000;
+    double principal = 2000;
 
-        double rate = 12;
+    double rate = 12;
 
-        double time = 1;
+    double time = 1;
 
-        rate = rate / (12 * 100);
+    rate = rate / (12 * 100);
 
-        time = time * 12;
+    time = time * 12;
 
-        double emi = (principal * rate * Math.pow(1 + rate, time)) / (Math.pow(1 + rate, time) - 1);
+    double emi = (principal * rate * Math.pow(1 + rate, time)) / (Math.pow(1 + rate, time) - 1);
 
-        System.out.print(" EMI is= " + emi + "\n");
+    System.out.print(" EMI is= " + emi + "\n");
 
-    }
+  }
 }
-}s
+}
 
